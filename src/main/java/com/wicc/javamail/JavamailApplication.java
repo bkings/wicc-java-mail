3f67d01134fad9d0a27f4d591c81a8b3123c058b
bkings <bbikings@yahoo.com>
2022-03-15 21:51:03 +0545

    fix: build file naming and config changes

--- src/main/java/com/wicc/javamail/JavamailApplication.java
+++ src/main/java/com/wicc/javamail/JavamailApplication.java
@@ -2,9 +2,15 @@ package com.wicc.javamail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavamailApplication {
+public class JavamailApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JavamailApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavamailApplication.class, args);
